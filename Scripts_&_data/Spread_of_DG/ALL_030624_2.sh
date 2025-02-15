38b3b6e79e1be6f17d2000829b2f755b0131324b
Simon Dellicour <simon.dellicour@ulb.be>
2025-02-15 16:59:45 +0100

    updating repo

--- Scripts_&_data/Spread_of_DG/ALL_030624_2.sh
+++
@@ -1,7 +0,0 @@
-# mafft ALL_030624_1.fas > ALL_030624_1_NEW.fas
-# mafft ALL_030624_2.fas > ALL_030624_2_NEW.fas
-
-IQTREE_v_1_6_12/iqtree -s ALL_030624_2.fas -m MFP -mem 10Go -mset GTR -nt 4 # -b 100
-
-BEAGLEPATH="/usr/local/lib/"
-java -Djava.library.path="$BEAGLEPATH" -jar beast_1_10_5.jar ALL_030624_2.xml
---
+++ Scripts_&_data/Spread_of_DG/IQTREE_analysis.sh
@@ -0,0 +1,1 @@
+IQTREE_v_1_6_12/iqtree -s ALL_021224_2.fas -m MFP -mem 10Go -mset GTR -nt 4
